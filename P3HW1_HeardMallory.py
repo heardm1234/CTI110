aaa25745059319947786d90e1471f157dd6e53e3
heardm1234 <124412325+heardm1234@users.noreply.github.com>
2023-03-10 20:47:01 -0500

    Add files via upload

---
+++ P3HW1_HeardMallory.py
@@ -0,0 +1,30 @@
+#CTI-110
+#P2HW2-List
+#Mallory Heard
+#26 FEB 23
+#
+myList = []
+
+for i in range(6):
+    a = float(input('Enter grade for Module '+str(i+1)+': '))
+    myList.append(a)
+  
+#print list
+print('\n\n----------Results----------')
+print('Lowest Grade:      '+str(min(myList)))
+print('Highest Grade:     '+str(max(myList)))
+print('Sum of Grades:     '+str(sum(myList)))
+Average = sum(myList)/len(myList)
+print(f'Average:           {Average:.2f} ')
+print('-------------------------------------')
+if Average >= 90: 
+  print('Your grade is: A')
+elif Average >= 80:
+  print('Your grade is: B')
+elif Average >= 70:
+  print('Your grade is: C')
+elif Average >= 60:
+  print('Your grade is: D')
+else:
+  print('Your grade is: F')
+  
